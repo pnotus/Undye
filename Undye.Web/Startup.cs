e659d9187da9816f2339f9b34b9b5be2d5f010e1
Pontus Frendin <pontus@frend.in>
2014-11-08 17:37:00 +0100

    Första incheckningen

---
+++ Undye.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Undye.Web.Startup))]
+namespace Undye.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
