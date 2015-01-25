2861a17d125ee045276dfd3613c5a95b43cbe3e8
Pontus Frendin <pontus@frend.in>
2015-01-25 13:56:05 +0100

    Lagt till möjlighet för en påloggad användare att "ta ställning"

--- Undye.Web/Controllers/HomeController.cs
+++ Undye.Web/Controllers/HomeController.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
+using Undye.Web.Models;
 
 namespace Undye.Web.Controllers
 {
     [RequireHttps]
     public class HomeController : Controller
     {
+        private ApplicationUserManager _userManager;
+
+        public HomeController()
+        {
+
+        }
+
+        public HomeController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -27,5 +55,42 @@ namespace Undye.Web.Controllers
 
             return View();
         }
+
+        [Authorize]
+        public async Task<ActionResult> Sign()
+        {
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            var model = new PetitionViewModel() { Signed = user.Signed };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<ActionResult> Sign(PetitionViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            user.Signed = model.Signed;
+
+            var result = await UserManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                AddErrors(result);
+            }
+            return RedirectToAction("Index");
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
     }
 }
---
+++ Undye.Web/Models/PetitionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Undye.Web.Models
+{
+    public class PetitionViewModel
+    {
+        public bool Signed { get; set; }
+    }
+}
